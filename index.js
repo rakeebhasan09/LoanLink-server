63aa39e6bcbc01ee9dc8273ee00c7c0ca3cedafb
rakeebhasan <mdrakeebhasan1@gmail.com>
2025-12-12 10:32:37 +0600

    Successfully connected with mongoDB

--- index.js
+++ index.js
@@ -5,13 +5,41 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 // Middlewares
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.x65kkeb.mongodb.net/?appName=Cluster0`;
+
 app.get("/", (req, res) => {
 	res.send("LoanLink server always running!");
 });
+
+const client = new MongoClient(uri, {
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
+});
+
+async function run() {
+	try {
+		// Connect the client to the server	(optional starting in v4.7)
+		await client.connect();
+		// Send a ping to confirm a successful connection
+		await client.db("admin").command({ ping: 1 });
+		console.log(
+			"Pinged your deployment. You successfully connected to MongoDB!"
+		);
+	} finally {
+		// Ensures that the client will close when you finish/error
+		await client.close();
+	}
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
